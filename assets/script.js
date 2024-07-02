dc079f6b453cfabf76722b52e73277c081ae8bc7
Maxence Gaucheron <maxence.mvfg@gmail.com>
2024-07-02 11:44:54 +0200

    Ajout d'event listeners sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,32 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Clic sur la flèche gauche
+
+let arrow_left = document.querySelector(".arrow_left");
+arrow_left.addEventListener("click", function () {
+	console.log("Vous avez cliqué sur la flèche gauche");
+});
+
+// Clic sur la flèche droite
+
+let arrow_right = document.querySelector(".arrow_right");
+arrow_right.addEventListener("click", function () {
+	console.log("Vous avez cliqué sur la flèche droite");
+});
